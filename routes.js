61e2e7498c1d734b471b7e19afa06cfe8ebee8fe
zero101010 <igotaraujo97@gmail.com>
2022-08-23 00:36:35 -0300

    change route

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 
 
 routes.get("/", (req,res)=>{
-    return res.json({"Title":"Welcome to simple Api","Status":"Up","Version":"2"}); 
+    return res.json({"Title":"Welcome to simple Api","Status":"Up","Version":"3"}); 
 });
 
 module.exports = routes;
